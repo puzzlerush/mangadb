2ef821b6a49237ab69965a72a938d5ee72bf4a61
tacticaltofu <dingkevin@gmail.com>
2021-03-03 00:28:23 -0500

    auto generate tsvector column when new rows are added

--- src/db/fulltextsearch.sql
+++ src/db/fulltextsearch.sql
@@ -43,3 +43,47 @@ setweight(to_tsvector(array_to_string(relation_titles, ' ')), 'B') ||
 setweight(to_tsvector(description), 'C')
 @@ plainto_tsquery('tower of god')
 ORDER BY rank DESC;
+
+/* Create separate tsvector column to improve search speed */
+ALTER TABLE manga ADD COLUMN textsearchable_index_col tsvector;
+UPDATE manga SET textsearchable_index_col =
+  setweight(to_tsvector(title), 'A') ||
+  setweight(to_tsvector(array_to_string(alttitles, ' ')), 'B') ||
+  setweight(to_tsvector(array_to_string(relation_titles, ' ')), 'B') ||
+  setweight(to_tsvector(description), 'C');
+
+/* Create stored generated column to hold tsvector */
+
+/* First create immutable version of array_to_string */
+create or replace function immutable_array_to_string(arr anyarray, delimiter text)
+returns text
+LANGUAGE plpgsql immutable
+as
+$$
+declare
+  concatenated text;
+begin
+  select array_to_string(arr, delimiter) into concatenated;
+  return concatenated;
+end;
+$$;
+
+/* Now to create stored generated column */
+ALTER TABLE manga
+  ADD COLUMN textsearchable_index_col tsvector
+    GENERATED ALWAYS AS (
+      setweight(to_tsvector('english', coalesce(title, '')), 'A') ||
+      setweight(to_tsvector('english', immutable_array_to_string(alttitles, ' ')), 'B') ||
+      setweight(to_tsvector('english', immutable_array_to_string(relation_titles, ' ')), 'B') ||
+      setweight(to_tsvector('english', coalesce(description, '')), 'C')
+    ) STORED;
+
+/* Insert new row to test the generated column */
+insert into manga (manga_id, title, alttitles, description, relation_titles)
+values (
+	69696969, 
+	'cory in the house', 
+	'{"white house livin", "tomato town"}', 
+	'disney classic anime',
+	'{"cory in the house 2"}'
+);
