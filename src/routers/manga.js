a580234e6938c32a793875487b3e0b4676f2dce3
tacticaltofu <dingkevin@gmail.com>
2021-03-05 16:52:33 -0500

    remove try catch to see the errors

--- src/routers/manga.js
+++ src/routers/manga.js
@@ -5,26 +5,33 @@ const { convertToMangaDexFormat } = require('../utils')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  try {
-    const { limit = 10, skip = 0 } = req.query
-    const result = await query(
-      'SELECT manga_id, title, artist, author, description, rating_bayesian, views, mainCover '
+  // try {
+  //   const { limit = 10, skip = 0 } = req.query
+  //   const result = await query(
+  //     'SELECT manga_id, title, artist, author, description, rating_bayesian, views, mainCover '
+  //   + 'FROM manga ORDER BY title LIMIT $1 OFFSET $2',
+  //     [limit, skip]
+  //   )
+  //   res.send(result.rows.map((row) => convertToMangaDexFormat(row)))
+  // } catch (e) {
+  //   res.status(400).send(e)
+  // }
+  const { limit = 10, skip = 0 } = req.query
+  const result = await query(
+    'SELECT manga_id, title, artist, author, description, rating_bayesian, views, mainCover '
     + 'FROM manga ORDER BY title LIMIT $1 OFFSET $2',
-      [limit, skip]
-    )
-    res.send(result.rows.map((row) => convertToMangaDexFormat(row)))
-  } catch (e) {
-    res.status(400).send(e)
-  }
+    [limit, skip]
+  )
+  res.send(result.rows.map((row) => convertToMangaDexFormat(row)))
 })
 
 router.get('/search', async (req, res) => {
   const searchStatement =
     `SELECT manga_id, title, artist, author, description, rating_bayesian, views, mainCover, COUNT(*) OVER() AS count `
-  + `FROM manga `
-  + `WHERE textsearchable_index_col @@ plainto_tsquery($1) `
-  + `ORDER BY ts_rank_cd(textsearchable_index_col, plainto_tsquery($1)) DESC `
-  + `LIMIT $2 OFFSET $3`
+    + `FROM manga `
+    + `WHERE textsearchable_index_col @@ plainto_tsquery($1) `
+    + `ORDER BY ts_rank_cd(textsearchable_index_col, plainto_tsquery($1)) DESC `
+    + `LIMIT $2 OFFSET $3`
   try {
     const { q: searchQuery, limit = 10, skip = 0 } = req.query
     const searchResult = await query(searchStatement, [searchQuery, limit, skip])
